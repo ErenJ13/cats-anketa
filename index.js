f322141d1daac66590a890b7e3da26d26e772d2d
Anastasia Gorkova <62338132+ErenJ13@users.noreply.github.com>
2022-05-01 16:43:38 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+class Cat {
+    constructor(petname, name, phone, adress, breed, food, sex) {
+        this.petname = petname;
+        this.name = name;
+        this.phone = phone;
+        this.adress = adress;
+        this.breed = breed;
+        this.food = food;
+        this.sex = sex;
+
+    }
+}
+
+function cats() {
+    let catname = document.getElementById('nickname').value;
+    let manname = document.getElementById('name').value;
+    let manphone = document.getElementById('phone').value;
+    let manadress = document.getElementById('adress').value;
+    let catbreed = document.querySelector('input[class="poroda"]:checked').value;
+    let catfood = document.querySelector('input[class="food"]:checked').value;
+    let catsex = document.querySelector('input[class="sex"]:checked').value;
+
+    let cat = new Cat(catname, manname, manphone, manadress, catbreed, catfood, catsex);
+
+    console.log(cat.petname);
+    console.log(cat.name);
+    console.log(cat.phone);
+    console.log(cat.adress);
+    console.log(cat.poroda);
+    console.log(cat.food);
+    console.log(cat.sex);
+}
